81b4b5e856db2755a89f4c30c9830a3ce98562d1
saumyapandyaa <pandyasaumya26@gmail.com>
2023-09-27 14:33:07 -0400

    updated new testing

--- finish/system/src/test/java/it/io/openliberty/guides/system/SystemEndpointIT.java
+++ finish/system/src/test/java/it/io/openliberty/guides/system/SystemEndpointIT.java
@@ -9,17 +9,11 @@
  * SPDX-License-Identifier: EPL-2.0
  *******************************************************************************/
 // end::copyright[]
+
 package it.io.openliberty.guides.system;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
-
-import javax.net.ssl.HostnameVerifier;
-import javax.net.ssl.SSLSession;
-import jakarta.ws.rs.client.Client;
-import jakarta.ws.rs.client.ClientBuilder;
-import jakarta.ws.rs.client.WebTarget;
-import jakarta.ws.rs.core.Response;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
@@ -27,33 +21,70 @@ import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
+import javax.net.ssl.HostnameVerifier;
+import javax.net.ssl.SSLSession;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
+
+import jakarta.ws.rs.client.ClientBuilder;
+import jakarta.ws.rs.core.Response;
+
+import org.jboss.resteasy.client.jaxrs.ResteasyClient;
+import org.jboss.resteasy.client.jaxrs.ResteasyClientBuilder;
+import org.jboss.resteasy.client.jaxrs.ResteasyWebTarget;
+
+import java.security.cert.CertificateException;
+import java.security.cert.X509Certificate;
+
 public class SystemEndpointIT {
 
     private static String clusterUrl;
 
-    private Client client;
+    // private Client client;
+    private ResteasyClient client;
     private Response response;
+    private SSLContext sslContext;
 
     @BeforeAll
     public static void oneTimeSetup() {
         String clusterIp = System.getProperty("cluster.ip");
         String nodePort = System.getProperty("system.node.port");
-
-        // clusterUrl = "http://" + clusterIp + ":" + nodePort + "/system/properties/";
         clusterUrl = "https://" + clusterIp + ":" + nodePort + "/system/properties/";
     }
 
     @BeforeEach
-    public void setup() {
-        response = null;
-        client = ClientBuilder.newBuilder()
-                    .hostnameVerifier(new HostnameVerifier() {
+    public void setup() { 
+        response=null;
+        try {
+            X509TrustManager customTrustManager = new X509TrustManager() {
+                @Override
+                public void checkClientTrusted(X509Certificate[] chain, String authType)
+                    throws CertificateException { }
+
+                @Override
+                public void checkServerTrusted(X509Certificate[] chain, String authType)
+                    throws CertificateException { }
+
+                @Override
+                public X509Certificate[] getAcceptedIssuers() {
+                    return null;
+                }
+            };
+            sslContext = SSLContext.getInstance("TLS");
+            sslContext.init(null, new TrustManager[] { customTrustManager }, null);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        ClientBuilder builder = ResteasyClientBuilder.newBuilder();
+        builder.sslContext(sslContext).hostnameVerifier(new HostnameVerifier() {
                         public boolean verify(String hostname, SSLSession session) {
                             return true;
                         }
-                    })
-                    .build();
-    }
+                    });
+        client = (ResteasyClient) builder.build();
+    }   
 
     @AfterEach
     public void teardown() {
@@ -63,8 +94,8 @@ public class SystemEndpointIT {
     @Test
     @Order(1)
     public void testPodNameNotNull() {
-        response = this.getResponse(clusterUrl);
-        this.assertResponse(clusterUrl, response);
+        response = this.getResponse();
+        this.assertResponse(response);
         String greeting = response.getHeaderString("X-Pod-Name");
 
         assertNotNull(greeting,
@@ -75,26 +106,18 @@ public class SystemEndpointIT {
     @Test
     @Order(2)
     public void testGetProperties() {
-        Client client = ClientBuilder.newClient();
-
-        WebTarget target = client.target(clusterUrl);
-        Response response = target.request().get();
-
+        response = this.getResponse();
         assertEquals(200, response.getStatus(),
                      "Incorrect response code from " + clusterUrl);
         response.close();
     }
 
-    private Response getResponse(String url) {
-        return client
-            .target(url)
-            .request()
-            .header("Host", System.getProperty("host-header"))
-            .get();
+    private Response getResponse(){
+        return client.target(clusterUrl).request().header("Host", System.getProperty("host-header")).get();
     }
 
-    private void assertResponse(String url, Response response) {
+    private void assertResponse(Response response) {
         assertEquals(200, response.getStatus(),
-            "Incorrect response code from " + url);
+            "Incorrect response code from " + clusterUrl);
     }
 }
